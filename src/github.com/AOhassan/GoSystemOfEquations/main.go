158fa223bb3e05f74cdde1672b0fecf5055e2087
Ahmed Osman <ahmedhosman41@gmail.com>
2019-02-01 14:09:28 -0800

    created structs

--- src/github.com/AOhassan/GoSystemOfEquations/main.go
+++ src/github.com/AOhassan/GoSystemOfEquations/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello, World")
+	fmt.Println(point{1,1})
+	fmt.Println(equation{1,2,3})
+}
+
+type point struct {
+	x float64
+	y float64
+}
+
+type equation struct {
+	x, y, c float64
 }
